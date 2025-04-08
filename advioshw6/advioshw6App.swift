b06a5926e3ad34ae4e3f4729156d24f0b0ec4def
diasjakupov <dias.jakupov@zimran.io>
2025-04-09 00:30:27 +0500

    Initial Commit

---
+++ advioshw6/advioshw6App.swift
@@ -0,0 +1,17 @@
+//
+//  advioshw6App.swift
+//  advioshw6
+//
+//  Created by Dias Jakupov on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct advioshw6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
